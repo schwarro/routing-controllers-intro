4a7a62331314b6c793eaa35ee4a7d5f1009b27cf
schwarro <rob.ap.schwartz@gmail.com>
2018-08-08 15:58:49 -0400

    completed kitten and kittens methods section

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-get '/'            => 'pages#welcome'
-get '/about'       => 'pages#about'
-get '/contest'     => 'pages#contest'
-get 'kitten/:size' => 'pages#kitten'
+  get '/'             => 'pages#welcome'
+  get '/about'        => 'pages#about'
+  get '/contest'      => 'pages#contest'
+  get 'kitten/:size'  => 'pages#kitten'
+  get 'kittens/:size' => 'pages#kittens'
 end
