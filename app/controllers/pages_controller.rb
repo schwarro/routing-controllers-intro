e75ff71fa934743a423b8dda72195eb9954ce224
schwarro <rob.ap.schwartz@gmail.com>
2018-08-08 15:48:24 -0400

    completed kitten method section

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,4 +10,9 @@ class PagesController < ApplicationController
   def contest
 
   end
+
+  def kitten
+    requested_size = params[:size]
+    @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+  end
 end
