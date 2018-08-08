1b0112e0ed8086ef75b6b369c04a545854d21265
schwarro <rob.ap.schwartz@gmail.com>
2018-08-08 15:01:39 -0400

    completed making the contest page work section

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-get '/' => 'pages#welcome'
+get '/'        => 'pages#welcome'
 get '/about'   => 'pages#about'
+get '/contest' => 'pages#contest'
 end
